978833740833fc193d8362de798c1def1a1f1071
aws-sbg-gcoek <awssbggcoek@gmail.com>
2026-09-16 03:08:51 +0530

    revert: discard 3D experience upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { AnimatePresence, motion } from 'motion/react';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import Scene3D from './components/Scene3D';
+import ParticleBackground from './components/ParticleBackground';
 import Home from './pages/Home';
 import About from './pages/About';
 import Events from './pages/Events';
@@ -61,7 +61,7 @@ export default function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-bg text-text-primary font-sans relative">
-        <Scene3D />
+        <ParticleBackground />
         <Navbar />
         <main className="flex-grow pt-24">
           <AnimatedRoutes />
